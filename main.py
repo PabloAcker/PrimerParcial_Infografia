925b70719b4457dd511d144979a3eba237ddda5a
PabloAcker <favw14@gmail.com>
2023-08-21 18:26:36 -0400

    commit con primer main hecho

--- main.py
+++ main.py
@@ -5,30 +5,30 @@ from frogger_game import FroggerGame
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Frogger Game"
+SPEED = 1
+
+class App(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        arcade.set_background_color(arcade.color.LIGHT_GREEN)
+
+    def on_draw(self):
+        arcade.start_render()
+        self.frog.draw() #TODO poner a los enemigos pero ver si es con un for
+
+    def on_update(self, delta_time: float):
+        self.frog.update(delta_time) #TODO cambiar esta linea y ver la logica de deteccion de enemigos del tank
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        if symbol in (arcade.key.UP, arcade.key.DOWN, arcade.key.LEFT, arcade.key.RIGHT):
+            self.frog.speed = SPEED
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        if symbol in (arcade.key.UP, arcade.key.DOWN, arcade.key.LEFT, arcade.key.RIGHT):
+            self.frog.speed = 0
 
 def main():
-    game = FroggerGame()
-    game.setup()
-
-    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-
-    @window.event
-    def on_draw():
-        arcade.start_render()
-        game.draw()
-
-    @window.event
-    def update(delta_time):
-        game.update(delta_time)
-
-    @window.event
-    def on_key_press(symbol, modifiers):
-        game.on_key_press(symbol, modifiers)
-
-    @window.event
-    def on_key_release(symbol, modifiers):
-        game.on_key_release(symbol, modifiers)
-
+    app = FroggerGame()
     arcade.run()
 
 if __name__ == "__main__":
